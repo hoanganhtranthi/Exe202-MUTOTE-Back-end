f7baa26f65a19d3c1ea637e4106156ce9037228d
hanh <hoanganhtranthi1610@gmail.com>
2023-09-23 09:05:49 +0700

    Update

--- MuTote.API/Program.cs
+++ MuTote.API/Program.cs
@@ -1,4 +1,5 @@
 using MuTote.API.Mapper;
+using MuTote.API.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware(typeof(GlobalErrorHandlingMiddleware));
 app.UseHttpsRedirection();
 app.UseCors("_myAllowSpecificOrigins");
 app.UseAuthorization();
